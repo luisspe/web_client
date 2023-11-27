0df48cae1b12af2fc612b0031417272ce3345c1c
luis peña <penagarcia.luisenrique@gmail.com>
2023-11-27 10:51:01 -0600

    notificaciones

--- clientes/models.py
+++ clientes/models.py
@@ -67,19 +67,4 @@ class Notificacion(models.Model):
         return f"Notificación para {self.usuario.username}: {self.mensaje}"
     
 
-@receiver(post_save, sender=Documento)
-def crear_notificacion_por_cambio_estado(sender, instance, **kwargs):
-    if kwargs.get('created', False):
-        return  # No hacer nada si el documento es nuevo
 
-    documento_anterior = Documento.objects.get(id=instance.id)
-    if documento_anterior.estado != instance.estado:
-        crear_notificacion_documento(instance)
-
-def crear_notificacion_documento(documento):
-    user_notification_settings = UserNotificationSettings.objects.get(user=documento.usuario)
-    if user_notification_settings.receive_notifications and user_notification_settings.notify_document_status:
-        Notificacion.objects.create(
-            usuario=documento.usuario,
-            mensaje=f"El estado de tu documento '{documento.tipo_documento}' ha cambiado a '{documento.estado}'."
-        )
--- v2rcx_client_web/settings.py
+++ v2rcx_client_web/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
-    'clientes',
+    'clientes.apps.ClientesConfig'
     
 ]
 
