3f31d9f1b789fedb90e8147588fd5c8b923355ef
luis peña <penagarcia.luisenrique@gmail.com>
2024-01-02 11:44:13 -0600

    fix subida de curp, spinner al carga de documentos

---
+++ administradores/static/assets/js/client_js/notifications.js
@@ -0,0 +1,25 @@
+function eliminarNotificacion(idNotificacion) {
+    var csrftoken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+    
+
+    fetch('/eliminar-notificacion/' + idNotificacion, {
+        method: 'POST',
+        headers: {
+            'X-CSRFToken': csrftoken
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            document.getElementById('notificacion-' + idNotificacion).remove();
+            window.location.reload();
+        } else {
+            alert('Error al eliminar la notificación');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+    
+    
+}
